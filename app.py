4328cbc9993e5c516a6b726ea3b45f76f207d458
Anton Topchii <anton.topchii@getbyrd.com>
2023-05-14 12:20:35 +0200

    Hello url_for

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -12,12 +11,18 @@ def index():
 @app.route("/greet/<name>")
 def greet(name):
     # modify this function to return "Hello, name"
-    return f"Hello, {'replace_me'}"
+    return f"Hello, {name}"
 
 
 @app.route("/multiply/<int:first_arg>/<int:second_arg>")
 def multiply(first_arg: int, second_arg:int):
-    return f"{'replace_me'}" # multiply args
+    return f"{first_arg * second_arg}" # multiply args
+
+
+with app.test_request_context():
+    print(url_for("index"))
+    print(url_for("multiply", first_arg=333, second_arg=3))
+    print(url_for("multiply", first_arg=333, second_arg=3, as_complex=True))
 
 
 if __name__ == "__main__":
