b0e5427cb86ce11890d177ca99e2c75b8e599f0e
Anton Topchii <anton.topchii@getbyrd.com>
2023-05-14 16:29:55 +0200

    Hello templates

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return  # Put any string here
+    return render_template("index.html")
+
+
+@app.route("/greet/<name>")
+def greet(name):
+    return render_template("greet.html", name=name)
+
+
+@app.route("/multiply/<int:first_arg>/<int:second_arg>")
+def multiply(first_arg: int, second_arg: int):
+    return f"{first_arg * second_arg}"
 
 
 if __name__ == "__main__":
