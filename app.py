031e32acfd58881fbc3fc4bb5f9e1a6f3a009acf
Anton Topchii <anton.topchii@getbyrd.com>
2023-05-14 12:13:39 +0200

    Url routing, and url converters

--- app.py
+++ app.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return  # Put any string here
+    return "Hello index page"
+
+
+@app.route("/greet/<name>")
+def greet(name):
+    # modify this function to return "Hello, name"
+    return f"Hello, {'replace_me'}"
+
+
+@app.route("/multiply/<int:first_arg>/<int:second_arg>")
+def multiply(first_arg: int, second_arg:int):
+    return f"{'replace_me'}" # multiply args
 
 
 if __name__ == "__main__":
